fc395d1d1ba1f90d7f794a4f49cc07891d45b197
Hardson TESSI <hardsontessi2@gmail.com>
2024-01-20 19:27:15 +0100

    Controller

--- src/main/java/com/hardsontessi/scratch/FirstController.java
+++ src/main/java/com/hardsontessi/scratch/FirstController.java
@@ -1,17 +1,18 @@
 package com.hardsontessi.scratch;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class FirstController {
 
+    @GetMapping("/hello/{userName}")
+    public String pathVar(@PathVariable("userName") String userName) {
+        return "My username: " + userName;
+    }
 
     @GetMapping("/hello")
-    public String sayHello() {
-        return "Hello from my first controller";
+    public String paramVar(@RequestParam("userName") String userName) {
+        return "My username: " + userName;
     }
 
     @PostMapping("/post-order")
