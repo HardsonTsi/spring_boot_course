1aa2d62ded0e8ba1f5df3c24233c7b48cac73216
Hardson TESSI <hardsontessi2@gmail.com>
2024-01-20 13:57:09 +0100

    Environment beans

--- src/main/java/com/hardsontessi/scratch/MyFirstService.java
+++ src/main/java/com/hardsontessi/scratch/MyFirstService.java
@@ -2,6 +2,7 @@ package com.hardsontessi.scratch;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,13 +10,33 @@ public class MyFirstService {
 
     private MyFirstClass myFirstClass;
 
-    @Autowired
-    public void setMyFirstClass(@Qualifier("mySecondClass") MyFirstClass myFirstClass) {
-        this.myFirstClass = myFirstClass;
-    }
+    private Environment environment;
+
 
     public String tellAStory() {
         return "The dependency is saying: " + myFirstClass.sayHello();
     }
 
+    public String getJavaVersion() {
+        return environment.getProperty("java.version");
+    }
+
+    public String getOS() {
+        return environment.getProperty("os.name");
+    }
+
+    public String readProp() {
+        return environment.getProperty("repo.author");
+    }
+
+
+    @Autowired
+    public void setMyFirstClass(@Qualifier("mySecondClass") MyFirstClass myFirstClass) {
+        this.myFirstClass = myFirstClass;
+    }
+
+    @Autowired
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
+    }
 }
--- src/main/java/com/hardsontessi/scratch/ScratchApplication.java
+++ src/main/java/com/hardsontessi/scratch/ScratchApplication.java
@@ -10,6 +10,9 @@ public class ScratchApplication {
         var ctx = SpringApplication.run(ScratchApplication.class, args);
         MyFirstService myFirstService = ctx.getBean(MyFirstService.class);
         System.out.println(myFirstService.tellAStory());
+        System.out.println(myFirstService.getJavaVersion());
+        System.out.println(myFirstService.getOS());
+        System.out.println(myFirstService.readProp());
     }
 
 }
